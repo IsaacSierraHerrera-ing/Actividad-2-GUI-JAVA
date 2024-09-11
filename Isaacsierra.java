186c67f17318272a24db1828bd5b061327f2a9ed
IsaacSierraHerrera-ing <sierraherreraisaac@gmail.com>
2024-09-11 13:01:10 -0500

    Add files via upload

---
+++ Isaacsierra.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package isaacsierra;
+
+
+import formulario.frmpersonas;
+
+/**
+ *
+ * @author jamer
+ */
+public class Isaacsierra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   frmpersonas H = new frmpersonas();
+        H.setVisible(true);
+        
+                
+                
+                
+                }    
+}
+
+    
+
